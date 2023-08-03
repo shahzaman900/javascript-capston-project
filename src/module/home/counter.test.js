343516547b344a4329fcc49d25601e3056c92ace
Fatima Najafi <najafifatima762@gmail.com>
2023-08-03 15:53:16 +0430

    upadte the counter test file

--- src/module/home/counter.test.js
+++ src/module/home/counter.test.js
@@ -1,10 +1,21 @@
-describe('counter Items', () => {
-  it('should count the number of items', async () => {
-    const countItems = await fetch(
-      'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood',
-    );
-    const data = await countItems.json();
-    const size = data.meals.length;
-    expect(size).toBe(28);
+const apiUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood';
+
+describe('Counter Items API', () => {
+  test('returns the correct number of items', async () => {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    expect(data.meals).toHaveLength(28);
+  });
+
+  test('does not return an incorrect number of items', async () => {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    expect(data.meals).not.toHaveLength(27);
+  });
+
+  test('returns non-empty data', async () => {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    expect(data.meals).not.toHaveLength(0);
   });
 });
