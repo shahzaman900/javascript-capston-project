c8e5ccce75dda0f1fa17badae5518a6d2cd708f5
sheezu900 <shahzamanlive2@gmail.com>
2023-08-04 16:38:36 +0500

    resolve error

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import getData from './module/home/getData.js';
 import countData from './module/home/counter.js';
 import getLike from './module/home/getLikes.js';
 
-
 const loadData = async () => {
   const data = await getData();
   const likes = await getLike();
@@ -14,7 +13,6 @@ const loadData = async () => {
       like = { item_id: item.idMeal, likes: 0 };
     }
     dataUi(item, like);
-    // urlComments = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Ix2mMVLk4DRU3xtKMCQq/comments?item_id=${ids}`;
   });
 };
 
