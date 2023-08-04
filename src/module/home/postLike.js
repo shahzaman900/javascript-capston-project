a31569d5327da708f063af35f68d2da7dfc23539
sheezu900 <shahzamanlive2@gmail.com>
2023-08-04 23:57:26 +0500

    resolve issue

--- src/module/home/postLike.js
+++ src/module/home/postLike.js
@@ -1,11 +1,8 @@
-/* eslint-disable quotes */
-// import loadData from '../../index.js';
-
-const urlLike = "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Ix2mMVLk4DRU3xtKMCQq/likes/";
+const urlLike = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Ix2mMVLk4DRU3xtKMCQq/likes/';
 const postLikes = async (like) => {
   const likes = await fetch(urlLike, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       item_id: like,
     }),
