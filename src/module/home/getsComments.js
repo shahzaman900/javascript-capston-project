2171d04d0e6e70fcc26e45daf44f26276db75cea
sheezu900 <shahzamanlive2@gmail.com>
2023-08-05 03:03:52 +0500

    resolve chnages

--- src/module/home/getsComments.js
+++
@@ -1,7 +0,0 @@
-// const getComments = async (like) => {
-//   const urlComments = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Ix2mMVLk4DRU3xtKMCQq/comments?item_id=${like.item_id}`;
-//   const comments = await fetch(urlComments).then((res) => res.json());
-//   return comments;
-// };
-
-// export default getComments;
